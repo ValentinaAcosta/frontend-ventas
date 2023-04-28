6abb2d200522c685c54c60ee93c8e89a0e82bf80
Valentina Acosta <124893622+ValentinaAcosta@users.noreply.github.com>
2023-04-28 13:59:36 -0500

    37. formulario de identificacion de usuarios con validacion de campos y 38. servicio de seguridad con conexion al backend

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioModel } from 'src/app/modelos/usuario.model';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import {MD5} from 'crypto-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identificacion-usuario',
@@ -6,5 +11,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./identificacion-usuario.component.css']
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+    private servicioSeguridad : SeguridadService,
+    private router: Router
+  ){
+
+  }
+
+  ngOnInit(){
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario(){
+    this.fGroup = this.fb.group({
+      usuario : ['', [Validators.required, Validators.email]],
+      clave : ['', [Validators.required ]]
+    });
+  }
+
+  IdentificarUsuario(){
+    if(this.fGroup.invalid){
+      alert('Datos incompletos')
+    }else{
+      let usuario= this.obtenerFormGroup['usuario'].value;
+      let clave= this.obtenerFormGroup['clave'].value;
+      let claveCifrada= MD5(clave).toString();
+
+    this.servicioSeguridad.IdentificarUsuario(usuario, claveCifrada).subscribe({
+      next: (datos:UsuarioModel) => {
+        console.log(datos);
+      },
+      error: (err) => {
+        console.log(err);  
+      }
+    })
+    }
+  }
+
+  get obtenerFormGroup(){
+    return this.fGroup.controls;
+  }
 
 }
