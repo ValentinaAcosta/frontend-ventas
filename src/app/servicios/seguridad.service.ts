8dd737671b0fe2f5321b27633de9939bc5ae6428
Valentina Acosta <124893622+ValentinaAcosta@users.noreply.github.com>
2023-04-28 16:15:45 -0500

    40. cambio de interfaz grafica con el inicio de sesion

--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -1,78 +1,112 @@
 import { Injectable } from '@angular/core';
 import { UsuarioModel } from '../modelos/usuario.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ConfiguracionRutasBackend } from '../config/configuracion.rutas.backend';
+import { UsuarioValidadoModel } from '../modelos/usuario.validado.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeguridadService {
-  urlBase : string = ConfiguracionRutasBackend.urlSeguridad;
+  urlBase: string = ConfiguracionRutasBackend.urlSeguridad;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.validacionDeSesion();
+  }
 
-/**
- * Identificar Usuario
- * @param usuario 
- * @param clave 
- * @returns datos del usuario validado
- */
-
- /**
+  /**
    * Identificar usuario
    * @param usuario 
    * @param clave 
    * @returns datos del usuario validado
    */
- IdentificarUsuario(usuario: string, clave: string): Observable<UsuarioModel> {
-  return this.http.post<UsuarioModel>(`${this.urlBase}identificar-usuario`, {
-    correo: usuario,
-    clave: clave
-  });
-}
-
-  /**
- * Almacena los datos del usuario
- * @param datos datos del usuario
- */
-AlmacenarDatosUsuarioIdentificado(datos: UsuarioModel): boolean{
-  let cadena = JSON.stringify(datos);
-  let datosLS = localStorage.getItem("datos-usuario");
-  if(datosLS){
-    return false;
-  } else{
-    localStorage.setItem("datos-usuario", cadena);
-    return true;
-  }
-}
-
-
-/**
- * Busca los datos en localstorage de un usuario
- * @returns 
- */
-ObtenerDatosUsuarioLS(): UsuarioModel | null{
-  let datosLS = localStorage.getItem("datos-usuario");
-  if (datosLS){
-    let datos = JSON.parse(datosLS);
-    return datos
-
-  }else{
-    return null;
-  }
-}
-
-  /**
- * Identificar usuario
- * @param usuario 
- * @param clave 
- * @returns datos del usuario validado
- */
-  ValidarCodigo2FA(idUsuario: string, codigo: string): Observable<object> {
-    return this.http.post<object>(`${this.urlBase}verificar-2fa`, {
-      usuarioId: idUsuario,
-      codigo2fa: codigo
+  IdentificarUsuario(usuario: string, clave: string): Observable<UsuarioModel> {
+    return this.http.post<UsuarioModel>(`${this.urlBase}identificar-usuario`, {
+      correo: usuario,
+      clave: clave
     });
   }
+
+    /**
+   * Almacena los datos del usuario
+   * @param datos datos del usuario
+   */
+  AlmacenarDatosUsuarioIdentificado(datos: UsuarioModel): boolean{
+    let cadena = JSON.stringify(datos);
+    let datosLS = localStorage.getItem("datos-usuario");
+    if(datosLS){
+      return false;
+    } else{
+      localStorage.setItem("datos-usuario", cadena);
+      return true;
+    }
+  }
+
+
+  /**
+   * Busca los datos en localstorage de un usuario
+   * @returns 
+   */
+  ObtenerDatosUsuarioLS(): UsuarioModel | null{
+    let datosLS = localStorage.getItem("datos-usuario");
+    if (datosLS){
+      let datos = JSON.parse(datosLS);
+      return datos
+
+    }else{
+      return null;
+    }
+  }
+
+ /**
+  * Validar 2fa
+  * @param idUsuario 
+  * @param codigo 
+  * @returns 
+  */
+    ValidarCodigo2FA(idUsuario: string, codigo: string): Observable<UsuarioValidadoModel> {
+      return this.http.post<UsuarioValidadoModel>(`${this.urlBase}verificar-2fa`, {
+        usuarioId: idUsuario,
+        codigo2fa: codigo
+      });
+    }
+
+
+    /**
+   * Guarda en local storage la información del usuario validado
+   * @param datos datos del usuario validado
+   * @returns respuesta
+   */
+    AlmacenarDatosUsuarioValidado(datos: UsuarioValidadoModel): boolean {
+      let datosLS = localStorage.getItem("datos-sesion");
+      if (datosLS != null) {
+        return false;
+      } else {
+        let datosString = JSON.stringify(datos);
+        localStorage.setItem("datos-sesion", datosString);
+        return true;
+      }
+    }
+
+
+    /**Administracion de la sesion de usuario */
+
+    datosUsuarioValidado = new BehaviorSubject<UsuarioValidadoModel>(new UsuarioValidadoModel());
+
+    ObtenerDatosSesion():Observable<UsuarioValidadoModel>{
+      return this.datosUsuarioValidado.asObservable();
+    }
+
+    validacionDeSesion(){
+      let ls = localStorage.getItem("datos-sesion");
+      if(ls){
+        let objUsuario = JSON.parse(ls);
+        this.ActualizarComportamientoUsuario(objUsuario);
+      }
+    }
+
+    ActualizarComportamientoUsuario(datos: UsuarioValidadoModel){
+      return this.datosUsuarioValidado.next(datos);
+    }
 }
